113f50eb4bd4f3719ec4733a7613d49599e8812d
Jacob Cambell <jacob@jacobcambell.com>
2021-12-14 18:42:57 -0700

    Show listings in index

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
-import React from 'react'
-import Navbar from '../components/Navbar/Navbar'
+import React from "react";
+import Navbar from "../components/Navbar/Navbar";
+import Listings from "../components/Listings/Listings";
 
 export default function Index() {
   return (
     <div>
       <Navbar></Navbar>
+      <Listings />
     </div>
-  )
+  );
 }
