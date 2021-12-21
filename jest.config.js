94376da5a42fdcd5aea5089476bd8a0a888c6752
Jacob Cambell <jacob@jacobcambell.com>
2021-12-21 12:12:43 -0700

    Allow tests to work with imported css

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,7 @@
 module.exports = {
   setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
   testPathIgnorePatterns: ["<rootDir>/.next/", "<rootDir>/node_modules/"],
+  moduleNameMapper: {
+    "^.+\\.(css|less|scss)$": "babel-jest",
+  },
 };
